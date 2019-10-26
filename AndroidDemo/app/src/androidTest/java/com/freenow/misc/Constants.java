28bd0404068cafb4ff75b28c82e2e453270e36fe
Mahmoud Abuzaid <mahmoud.a.zaid@outlook.com>
2019-10-26 19:00:08 +0300

    Add constant for UiDevice

--- AndroidDemo/app/src/androidTest/java/com/freenow/misc/Constants.java
+++ AndroidDemo/app/src/androidTest/java/com/freenow/misc/Constants.java
@@ -1,4 +1,11 @@
 package com.freenow.misc;
 
 public class Constants {
+
+    private Constants(){
+
+    }
+
+    public static final String DIALER_PACKAGE = "com.google.android.dialer";
+    public static final long LAUNCH_TIMEOUT = 5000L;
 }
