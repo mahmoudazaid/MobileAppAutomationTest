25e223ca4a8a808caafc003f6340032c95157016
Mahmoud Abuzaid <mahmoud.a.zaid@outlook.com>
2019-10-28 06:39:02 +0300

    Comment one Rule

--- AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/AuthenticationTest.kt
+++ AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/AuthenticationTest.kt
@@ -39,8 +39,8 @@ class AuthenticationTest {
     @get:Rule
     var mainActivityRule = ActivityTestRule(MainActivity::class.java)
 
-    @get:Rule
-    var activityRule = ActivityTestRule(AuthenticationActivity::class.java)
+//    @get:Rule
+//    var activityRule = ActivityTestRule(AuthenticationActivity::class.java)
 
     //endregion
 
@@ -48,7 +48,7 @@ class AuthenticationTest {
     fun login() {
         // Initialize UiDevice instance
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-
+        
         //Check if the username text field has been displayed
         onView(withId(R.id.edt_username)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         //Enter Username
@@ -60,7 +60,7 @@ class AuthenticationTest {
         //Check if th the login has been displayed
         onView(withId(R.id.btn_login)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         //Click on Login
-        Thread.sleep(5000)
+//        Thread.sleep(5000)
         onView(withId(R.id.btn_login)).perform(click())
         Thread.sleep(10000)
 
